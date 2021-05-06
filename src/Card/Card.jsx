362b2cb58e1ffb1a7afea5dbafb087f9e0111ef7
sbmaxx <sbmaxx@gmail.com>
2021-05-07 00:34:39 +0300

    removed reduntant div

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -65,61 +65,59 @@ class CardSide extends React.Component {
                 className={`card__side card__side_lang_${lang} card__side_state_${index === 0 ? 'opened' : 'closed'} card__layout`}
                 data-lang={lang}
                 itemScope itemType="http://data-vocabulary.org/Person">
-                <div className="card__content card__rectangle">
-                    <a className="card__logo" target="_blank" rel="noopener noreferrer" href={company.site}><span itemProp="affiliation">{company.name}</span></a>
-                    <div className="card__text">
-                        <div className="card__title">
-                            <h1 className="card__name" itemProp="name">{name}</h1>
-                            <div className="card__position" itemProp="title">{position}</div>
-                        </div>
-                        <div className="card__address" itemProp="address" itemScope itemType="http://data-vocabulary.org/Address">
-                            {lang === 'ru' && (
-                                <>
-                                    <span className="card__country" itemProp="country-name">{address.country}</span>,{' '}
-                                    <span className="card__city" itemProp="locality">{address.city}</span>,{' '}
-                                    <span className="card__zip" itemProp="postal-code">{address.zip}</span>
-                                    <br />
-                                    <span className="card__street-address" itemProp="street-address">{address['street-address']}</span>
-                                </>)}
-                            {lang === 'en' && (
-                                <>
-                                    <span className="card__street-address" itemProp="street-address">{address['street-address']}</span>
-                                    <br />
-                                    <span className="card__city" itemProp="locality">{address.city}</span>,{' '}
-                                    <span className="card__zip" itemProp="postal-code">{address.zip}</span>,{' '}
-                                    <span className="card__country" itemProp="country-name">{address.country}</span>
-                                </>
-                            )}
-                        </div>
-                        <div className="card__contact">
-                            {(contact.work && !contact.workExt) &&<div className="card__tel card__tel_type_work">{I18N[lang].tel} <a className="card__phone-link"
-                                href={`tel:${rawNumber(contact.work)}`}>{contact.work}</a></div>}
+                <a className="card__logo" target="_blank" rel="noopener noreferrer" href={company.site}><span itemProp="affiliation">{company.name}</span></a>
+                <div className="card__text">
+                    <div className="card__title">
+                        <h1 className="card__name" itemProp="name">{name}</h1>
+                        <div className="card__position" itemProp="title">{position}</div>
+                    </div>
+                    <div className="card__address" itemProp="address" itemScope itemType="http://data-vocabulary.org/Address">
+                        {lang === 'ru' && (
+                            <>
+                                <span className="card__country" itemProp="country-name">{address.country}</span>,{' '}
+                                <span className="card__city" itemProp="locality">{address.city}</span>,{' '}
+                                <span className="card__zip" itemProp="postal-code">{address.zip}</span>
+                                <br />
+                                <span className="card__street-address" itemProp="street-address">{address['street-address']}</span>
+                            </>)}
+                        {lang === 'en' && (
+                            <>
+                                <span className="card__street-address" itemProp="street-address">{address['street-address']}</span>
+                                <br />
+                                <span className="card__city" itemProp="locality">{address.city}</span>,{' '}
+                                <span className="card__zip" itemProp="postal-code">{address.zip}</span>,{' '}
+                                <span className="card__country" itemProp="country-name">{address.country}</span>
+                            </>
+                        )}
+                    </div>
+                    <div className="card__contact">
+                        {(contact.work && !contact.workExt) &&<div className="card__tel card__tel_type_work">{I18N[lang].tel} <a className="card__phone-link"
+                            href={`tel:${rawNumber(contact.work)}`}>{contact.work}</a></div>}
 
-                            {(contact.work && contact.workExt) &&<div className="card__tel card__tel_type_work">{I18N[lang].tel} <a className="card__phone-link"
-                                href={`tel:${rawNumber(contact.work)},${contact.workExt}`}>{contact.work}</a>, {I18N[lang].ext} {contact.workExt}</div>}
+                        {(contact.work && contact.workExt) &&<div className="card__tel card__tel_type_work">{I18N[lang].tel} <a className="card__phone-link"
+                            href={`tel:${rawNumber(contact.work)},${contact.workExt}`}>{contact.work}</a>, {I18N[lang].ext} {contact.workExt}</div>}
 
-                            {contact.cell && <div className="card__tel card__tel_type_cell">{I18N[lang].cell} <a className="card__phone-link"
-                                href={`tel:${rawNumber(contact.cell)}`}>{contact.cell}</a></div>}
+                        {contact.cell && <div className="card__tel card__tel_type_cell">{I18N[lang].cell} <a className="card__phone-link"
+                            href={`tel:${rawNumber(contact.cell)}`}>{contact.cell}</a></div>}
 
-                            {(contact.work || contact.cell) && <div className="card__gap"></div>}
+                        {(contact.work || contact.cell) && <div className="card__gap"></div>}
 
-                            {contact.email && <div className="card__email">
-                                <CardLink href={`mailto:${contact.email}`}>{contact.email}</CardLink>
-                            </div>}
+                        {contact.email && <div className="card__email">
+                            <CardLink href={`mailto:${contact.email}`}>{contact.email}</CardLink>
+                        </div>}
 
-                            {contact.site && <div className="card__site">
-                                <CardLink href={contact.site} itemProp="url">{contact.site}</CardLink>
-                            </div>}
+                        {contact.site && <div className="card__site">
+                            <CardLink href={contact.site} itemProp="url">{contact.site}</CardLink>
+                        </div>}
 
-                            {(contact.email || contact.site) && <div className="card__gap"></div>}
+                        {(contact.email || contact.site) && <div className="card__gap"></div>}
 
-                            {contact.telegram && <div className="card__telegram">telegram: <CardLink href={`https://t.me/${contact.telegram}`}><span
-                                itemProp="nickname">{contact.telegram}</span></CardLink></div>}
+                        {contact.telegram && <div className="card__telegram">telegram: <CardLink href={`https://t.me/${contact.telegram}`}><span
+                            itemProp="nickname">{contact.telegram}</span></CardLink></div>}
 
-                            {contact.github && <div className="card__github">
-                                <CardLink href={`https://github.com/${contact.github}`}>github.com/{contact.github}</CardLink>
-                            </div>}
-                        </div>
+                        {contact.github && <div className="card__github">
+                            <CardLink href={`https://github.com/${contact.github}`}>github.com/{contact.github}</CardLink>
+                        </div>}
                     </div>
                 </div>
             </div>
