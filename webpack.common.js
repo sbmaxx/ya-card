a81f1c70f0adf9a49bd794d894a75dec9a3c4377
sbmaxx <sbmaxx@yandex-team.ru>
2021-05-06 15:58:58 +0300

    postcss & stylelint

--- webpack.common.js
+++ webpack.common.js
@@ -51,6 +51,7 @@ module.exports = {
                     },
                   },
                   'css-loader',
+                  "postcss-loader"
                 ],
             },
         ],
